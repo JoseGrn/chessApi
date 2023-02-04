c78439f58067f8aa902ded2cd476cc5a07132193
josedangrn01 <josedanielgrn01@gmail.com>
2023-02-03 18:26:16 -0600

    cambios en post de game

--- dataAccess/models/clsGameEntityModel.cs
+++ dataAccess/models/clsGameEntityModel.cs
@@ -16,9 +16,9 @@ public sealed class clsGameEntityModel<TI, TC> : relationalEntity<TI, TC>
 
     public TI id { get; set; }
     public DateTime started { get; set; }
-    public int whites { get; set; }
-    public int blacks { get; set; }
+    public int? whites { get; set; }
+    public int? blacks { get; set; }
     public bool turn { get; set; }
-    public int winner { get; set; }
+    public int? winner { get; set; }
     public override TI key { get => id; set => id = value; }
 }
